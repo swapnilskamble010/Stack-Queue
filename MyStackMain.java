59e0e96d8b037f1fabcc8baefba5a1d9fd4b2676
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-04-02 22:31:52 +0530

    Welcome message added

---
+++ MyStackMain.java
@@ -0,0 +1,8 @@
+package com.bl.ds.stack_queue;
+
+public class MyStackMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack & Queue Implementation Program with the help of Linked List");
+	}
+}
